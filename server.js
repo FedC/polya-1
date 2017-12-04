2ca58bfde865093306fad4266687003b71cd6736
Federico Commisso <fdcommisso@gmail.com>
2017-12-03 19:03:13 -0500

    express server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('*.*', express.static(path.join(process.cwd()), {
+  maxAge: '1y'
+}));
+
+app.listen(PORT, () => {
+  console.log(`Node Express server listening on http://localhost:${PORT}`);
+});
