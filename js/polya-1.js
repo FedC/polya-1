8fc4b9339f5985be003d4cc4f9664e2d5e6d020a
Federico Commisso <fdcommisso@gmail.com>
2017-12-03 23:45:41 -0500

    (feat): accuracy

--- js/polya-1.js
+++ js/polya-1.js
@@ -1,3 +1,5 @@
+var TOLERANCE = 1.0;
+
 var DATA = {
 	1: [], // data set 1
 	2: [] // data set 2
@@ -10,13 +12,42 @@ var DATAX = 'Knee Int/Ext R.';
 var DATAY = 'Ankle Flex/Ext';
 
 $('#loading').hide();
+$('#analysis').hide();
 
+$("#analyze").on('click', function (event) {
+  analyzeData();
+});
 
+$('#tolerance').on('input', function(event) {
+  console.log('tolerance change: ', event.target.value);
+  TOLERANCE = event.target.value;
+});
 
-chart_data = [];
+$('textarea').on('input', function(event) {
 
-DATA[1].map( (d,i) => {
- if (i%3==0) chart_data.push({ "Ankle Flex/Ext": data[i], "Knee Int/Ext R.": data[i+1]});
+  let id = $(event.target).attr('data-id');
+
+  DATA[ id ] = [];
+
+  let data = event.target.value.split('\t');
+
+  // console.log(data);
+
+  if (data[0] == 'Tiempo' ) {
+    // session description was erased from text file
+    data.shift();
+    data.shift();
+    data.shift();
+    data.shift();
+  }
+  else if (data[0].length > 10) data.shift(); // session description is split into index 0
+
+  data.map( (d,i) => {
+   if (i%3==0) DATA[ id ].push({ "Ankle Flex/Ext": data[i], "Knee Int/Ext R.": data[i+1]});
+  });
+
+  plot();
+  analyzeData();
 });
 
 
@@ -54,6 +85,8 @@ function handleFiles(delegate, files) {
 function collectDataFromFile(options) {
 	console.log("COLLECTING DATA...");
 
+  DATA[options.set] = [];
+
 	d3.tsv(options.url, function(error, data) {
 	  if (error) throw error;
 	  var data_error;
@@ -77,10 +110,70 @@ function collectDataFromFile(options) {
 
 	  console.log('DONE LOADING DATA:', DATA);
 	  plot();
+    analyzeData();
 
 	});
 
 }
+
+
+function analyzeData() {
+
+  $('#loading').show();
+  $('#chart').hide();
+  $('#analysis').hide();
+
+  setTimeout(function () {
+
+    if (DATA[2].length > 0 && DATA[1].length > 0) {
+
+      console.log('analyzeData');
+
+      DATA[2].forEach(d2 => {
+        let ankle2 = d2['Ankle Flex/Ext'];
+        let knee2 = d2['Knee Int/Ext R.'];
+
+        d2.inside = false;
+
+        DATA[1].forEach(d1 => {
+          let ankle1 = d1['Ankle Flex/Ext'];
+          let knee1 = d1['Knee Int/Ext R.'];
+
+          if ( distance( {x: ankle1, y: knee1}, {x: ankle2, y: knee2} ) <= TOLERANCE ) {
+            d2.inside = true;
+          }
+        });
+
+      });
+
+      let hits = 0;
+      DATA[2].forEach(d => {
+        if (d.inside) hits++;
+      });
+
+      let accuracy = parseFloat( hits / DATA[2].length * 100).toFixed(3);
+
+      $('#hits').html( hits + "/" + DATA[2].length );
+      $('#accuracy').html( accuracy + "%" );
+    }
+
+
+    $('#loading').hide();
+    $('#chart').show();
+    $('#analysis').show();
+
+  }, 100);
+}
+
+
+function distance(p1, p2) {
+  /*
+    Distance = √(x2−x1)^2+(y2−y1)^2
+  */
+  return Math.sqrt( Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2) );
+
+}
+
 
 
 function plot() {
