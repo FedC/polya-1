cdd34bc93cbb21d4941e88d4cab8e6ec0d425a72
Federico Commisso <fdcommisso@gmail.com>
2018-02-18 17:44:13 -0500

    (feat): updates

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  };
+};
+app.use(forceSSL());
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
